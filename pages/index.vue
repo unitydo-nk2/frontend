1492cb61fa54e4ff3ff0b458d2e1230459c72325
dreamqtk <dreamqtk@gmail.com>
2023-11-28 18:57:09 +0700

    [EDIT] carousel component

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <!-- @format -->
 <script setup>
 import NavBar from '~/components/NavBar.vue';
-import Carousel from '~/components/Carousel.vue';
+// import Carousel from '~/components/Carousel.vue';
 </script>
 
 <template>
