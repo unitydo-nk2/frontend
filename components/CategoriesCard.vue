6e8cd2380626ce8deb5361eec740aa8c12d57e6d
phxsrp <phxsrp@gmail.com>
2024-03-28 15:55:42 +0700

    [UPDATE] fix cannot find image

--- components/CategoriesCard.vue
+++ components/CategoriesCard.vue
@@ -12,7 +12,7 @@ const props = defineProps({
     <div
       class="w-52 h-24 px-5 py-3.5 bg-violet-50 hover:bg-indigo-600 hover:text-white rounded-xl shadow border border-indigo-600 justify-start items-center gap-5 flex"
     >
-      <img class="w-11 h-11" src="https://via.placeholder.com/46x46" />
+      <img class="w-11 h-11" src="/image/unityDoLogo.png"/>
       <div
         class="w-fit h-6 text-center  text-xl leading-7 tracking-wide"
       >
